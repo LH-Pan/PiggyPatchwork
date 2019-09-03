979942f88efa6a035ba60a9dc907f0f55b550699
潘立祥 <panlishiang@panlixiangde-MacBook-Pro.local>
2019-09-03 22:40:17 +0800

    禁止 app 可以轉向

--- PiggyPatchwork/AppDelegate.swift
+++ PiggyPatchwork/AppDelegate.swift
@@ -11,7 +11,7 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     // swiftlint:disable line_length
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
